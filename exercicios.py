1963767d70481d02bfa9cc6ffe0e58f99ac55067
Danilo R <danilo569@gmail.com>
2025-11-08 22:19:42 -0300

    terceiro exercício + tratamento de erros

--- exercicios.py
+++ exercicios.py
@@ -1,4 +1,4 @@
-
+import traceback
 # #### Inteiros (`int`)
 
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
@@ -16,35 +16,61 @@
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 
 
-import traceback
+
+# while True:
+#     try:
+#         resto_1 = float(input("Por favor insira um número para o cálculo de resto 5: "))
+#         resultado_resto_1 = resto_1 % 5
+
+#         if resultado_resto_1.is_integer():
+#             print(f"O resto da conta é: {int(resultado_resto_1)}")
+#             break
+#         else:
+#             print(f"{resultado_resto_1:.2f}")
+#             break
+
+#     except Exception as e:
+#         print("Ocorreu um erro:")
+#         traceback.print_exc()
+
+#         resposta= input("\nDeseja tentar novamente? (s/n): ").strip().lower()
+
+#         if resposta != "s":
+#             print("Programa encerrado. Até mais!")
+#             break
+#         else:
+#             print("\nReiniciando programa")
+
+
+
+# 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
+
 
 while True:
     try:
-        resto_1 = float(input("Por favor insira um número para o cálculo de resto 5: "))
-        resultado_resto_1 = resto_1 % 5
+        multi_1 = float(input("Digite o primeiro número da multplicação: "))
+        multi_2 = float(input("Digite o segundo número da multplicação: "))
+        resultado_multi = multi_1 * multi_2
 
-        if resultado_resto_1.is_integer():
-            print(f"O resto da conta é: {int(resultado_resto_1)}")
+        if resultado_multi.is_integer():
+            print(f"O resultado da multiplicação é: {int(resultado_multi)}")
             break
         else:
-            print(f"{resultado_resto_1:.2f}")
+            print(f"O resultado da multiplicação é: {resultado_multi}")
             break
-
-    except Exception as e:
-        print("Ocorreu um erro:")
+    except:
+        print("Aconteceu um erro: ")
         traceback.print_exc()
 
-        resposta= input("\nDeseja tentar novamente? (s/n): ").strip().lower()
+        resposta_multi= input("\nDeseja tentar novamente? (s/n): ").strip().lower()
 
-        if resposta != "s":
+        if resposta_multi != "s":
             print("Programa encerrado. Até mais!")
             break
         else:
             print("\nReiniciando programa")
 
-
-
-# 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
